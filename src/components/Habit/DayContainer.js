ce086f69093936c02ed759c6d54a097e6423e0c1
pedrorosadomingues@gmail.com <pedrorosadomingues@gmail.com>
2022-12-16 18:31:16 -0300

    Requisito: barra de progresso botao Hoje

---
+++ src/components/Habit/DayContainer.js
@@ -0,0 +1,26 @@
+import styled from "styled-components";
+
+export default function DayContainer({selectedDaysIndex, index, day, weekdays }) {
+    let selected = selectedDaysIndex.includes(index)
+    console.log(selected);
+    return (
+        <DayStyle selected={selected}>
+            {day.name}
+        </DayStyle>
+    )
+}
+
+const DayStyle = styled.div`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    margin-left: 3px;
+    width: 30px;
+    height: 30px;
+    border-radius: 5px;
+    border: 1px solid #D4D4D4;
+    color: ${props => props.selected ? "#FFFFFF" : "#DBDBDB"};
+    font-size: 20px;
+    font-family: 'Lexend Deca', sans-serif;
+    background: ${props => props.selected ? "#CFCFCF" : "#FFFFFF"};
+`
--- src/components/Habit/Habit.js
+++ src/components/Habit/Habit.js
@@ -1,15 +1,20 @@
 import styled from "styled-components";
+import DayContainer from "./DayContainer";
 import { useContext } from "react";
 import { UserContext } from "../../contexts/UserContext";
 
-
 export default function Habit({habit}) {
-    const {habits} = useContext(UserContext);
-    console.log(habit);
+    const { weekdays } = useContext(UserContext);
+    console.log(weekdays);
     return (
         <HabitStyle>
             <h1>{habit.name}</h1>
-            
+            <DaysContainerStyle>          
+            {weekdays.map((d, index) => {
+                return <DayContainer key={index} index={index+1} day={d} selectedDaysIndex={habit.days} />
+            })}
+        </DaysContainerStyle>
+            <ion-icon name="trash-outline"></ion-icon>
         </HabitStyle>
     )
 }
@@ -19,16 +24,34 @@ const HabitStyle = styled.div`
     height: 91px;
     background: #FFFFFF;
     border-radius: 5px;
-    margin: 10px auto;
+    margin: 10px auto 0 auto;
+    padding:15px;
+    position: relative;
+
     h1 {
         font-size: 18px;
         color: #666666;
         font-family: 'Lexend Deca', sans-serif;
         font-weight: 400;
         line-height: 22px;
-        margin-top: 30px;
         padding: 0 18px;
         max-width: 375px;
         text-align: start;
     }
+    ion-icon {
+        position: absolute;
+        right: 8px;
+        top: 8px;
+        font-size: 20px;
+        color: #666666;
+
+        :hover {
+            cursor: pointer;
+        }
+    }
+`
+const DaysContainerStyle = styled.div`
+    width: 100%;
+    display: flex;
+    margin-top: 10px;
 `
